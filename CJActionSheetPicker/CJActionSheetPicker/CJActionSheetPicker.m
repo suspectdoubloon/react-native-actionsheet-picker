90a80485ac06c5c15d9216bc6bc02688da8ef65b
Jarrad Littleford <jarrad@madeinkatana.com>
2017-06-15 14:39:01 +0930

    Removing multiple picker as its not clear from the docs

--- CJActionSheetPicker/CJActionSheetPicker/CJActionSheetPicker.m
+++ CJActionSheetPicker/CJActionSheetPicker/CJActionSheetPicker.m
@@ -6,7 +6,6 @@
 
 #import "AbstractActionSheetPicker.h"
 #import "ActionSheetStringPicker.h"
-#import "ActionSheetStringMultipleSelectionPicker.h"
 #import "ActionSheetDatePicker.h"
 
 @implementation CJActionSheetPicker
@@ -27,69 +26,46 @@ RCT_EXPORT_METHOD(showStringPicker:(NSDictionary *)options
         RCTLogError(@"Tried to display action sheet but there is no application window. options: %@", options);
         return;
     }
-
+    
     UIView *sourceView = controller.view;
-
+    
     NSString *title = @"";
     if (options[@"title"]) {
         title = [RCTConvert NSString:options[@"title"]];
     }
-
-    BOOL multiple = options[@"multiple"] ? YES : NO;
-
+    
+    
     NSArray *rows = [RCTConvert NSArray:options[@"rows"]];
-
+    
     void(^cancelBlock)(AbstractActionSheetPicker *) = ^(AbstractActionSheetPicker *picker) {
         resolve(@{ @"cancelled": @YES });
     };
-
+    
     AbstractActionSheetPicker *picker;
-
-    if (multiple) {
-        NSArray<NSNumber *> *selectedIndices = nil;
-        if (options[@"selectedIndices"]) {
-            selectedIndices = [RCTConvert NSNumberArray:options[@"selectedIndices"]];
-        }
-
-        void(^doneBlock)(ActionSheetStringMultipleSelectionPicker *, NSArray*, id) = ^(ActionSheetStringMultipleSelectionPicker *picker, NSArray *selectedIndices, id selectedValues) {
-            resolve(@{
-                @"cancelled": @NO,
-                @"selectedIndices": selectedIndices,
-                @"selectedValues": (NSArray *)selectedValues
-            });
-        };
-
-        picker = [[ActionSheetStringMultipleSelectionPicker alloc] initWithTitle:title
-                                                                            rows:rows
-                                                                initialSelection:selectedIndices
-                                                                       doneBlock:doneBlock
-                                                                     cancelBlock:cancelBlock
-                                                                          origin:sourceView];
-    } else {
-        NSInteger *selectedIndex = nil;
-        if (options[@"selectedIndex"]) {
-            selectedIndex = [RCTConvert NSInteger:options[@"selectedIndex"]];
-        }
-
-        void(^doneBlock)(ActionSheetStringPicker *, NSInteger, id) = ^(ActionSheetStringPicker *picker, NSInteger selectedIndex, id selectedValue) {
-            resolve(@{
-                @"cancelled": @NO,
-                @"selectedIndex": @(selectedIndex),
-                @"selectedValue": (NSString *)selectedValue
-            });
-        };
-
-        picker = [[ActionSheetStringPicker alloc] initWithTitle:title
-                                                           rows:rows
-                                               initialSelection:selectedIndex
-                                                      doneBlock:doneBlock
-                                                    cancelBlock:cancelBlock
-                                                         origin:sourceView];
+    
+    NSInteger *selectedIndex = nil;
+    if (options[@"selectedIndex"]) {
+        selectedIndex = [RCTConvert NSInteger:options[@"selectedIndex"]];
     }
-
+    
+    void(^doneBlock)(ActionSheetStringPicker *, NSInteger, id) = ^(ActionSheetStringPicker *picker, NSInteger selectedIndex, id selectedValue) {
+        resolve(@{
+                  @"cancelled": @NO,
+                  @"selectedIndex": @(selectedIndex),
+                  @"selectedValue": (NSString *)selectedValue
+                  });
+    };
+    
+    picker = [[ActionSheetStringPicker alloc] initWithTitle:title
+                                                       rows:rows
+                                           initialSelection:selectedIndex
+                                                  doneBlock:doneBlock
+                                                cancelBlock:cancelBlock
+                                                     origin:sourceView];
+    
     // disable popover for now to support ipad.
     picker.popoverDisabled = true;
-
+    
     [picker showActionSheetPicker];
 }
 
@@ -102,36 +78,36 @@ RCT_EXPORT_METHOD(showCountDownPicker:(NSDictionary *)options
         RCTLogError(@"Tried to display action sheet but there is no application window. options: %@", options);
         return;
     }
-
+    
     UIView *sourceView = controller.view;
-
+    
     NSString *title = @"";
     if (options[@"title"]) {
         title = [RCTConvert NSString:options[@"title"]];
     }
-
+    
     void(^doneBlock)(ActionSheetDatePicker *, id, id) = ^(ActionSheetDatePicker *picker, id selectedDate, id origin) {
         resolve(@{ @"cancelled": @NO, @"countDownDuration": selectedDate });
     };
-
+    
     void(^cancelBlock)(ActionSheetDatePicker *) = ^(ActionSheetDatePicker *picker) {
         resolve(@{ @"cancelled": @YES });
     };
-
+    
     ActionSheetDatePicker *datePicker = [[ActionSheetDatePicker alloc] initWithTitle:title
                                                                       datePickerMode:UIDatePickerModeCountDownTimer
                                                                         selectedDate:nil
                                                                            doneBlock:doneBlock
                                                                          cancelBlock:cancelBlock
                                                                               origin:sourceView];
-
+    
     if (options[@"countDownDuration"]) {
         datePicker.countDownDuration = [RCTConvert double:options[@"countDownDuration"]];
     }
-
+    
     // disable popover for now to support ipad.
     datePicker.popoverDisabled = true;
-
+    
     [datePicker showActionSheetPicker];
 }
 
